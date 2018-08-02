ba4833e581438afc979a7c1b97ba72477bb7b831
hzmdschhc <hzmdschhc11@163.com>
2018-08-02 18:32:01 +0800

    添加了按<F4>批量注释的功能，目前只支持python的#

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 ## Install
 run ./install.py
 
-It will backup ~/.vimrc to vimrc.bak, and then copy vimrc to ~/.vimrc
+It will: 
 
-if ./colors/ has *.vim files, it will copy *.vim to ~/.vim/colors
+1. backup ~/.vimrc to vimrc.bak, and then copy vimrc to ~/.vimrc
+2. if ./colors/ has *.vim files, it will copy *.vim to ~/.vim/colors
+3. install Vundle if you want
 
 ## Uninstall
 run ./uninstall.sh
--- install.py
+++ install.py
@@ -4,27 +4,90 @@ import os
 import getpass
 import shutil
 
-vimrc_path = '/home/{user}/.vimrc'.format(user=getpass.getuser())
+# Vundle
+def vundle_install(target_path):
+    if  os.path.exists(vundle_path):
+        print('Vundle: Found')
+    else:
+        print('Vundle: Not found')
+        print('Vundle: Installing...')
+        os.system('git clone https://github.com/VundleVim/Vundle.vim.git'
+            + ' ~/.vim/bundle/Vundle.vim')
+        print('Vundle: Plugin installing...')
+        os.system('vim +PluginInstall +qall')
+        print('Vundle: Plugin install done')
+        print('Vundle: done\n')
 
-if os.path.exists(vimrc_path):
-    print('file:', vimrc_path, 'has existsed')
-    print('Back up it to vimrc/vimrc.bak')
-    shutil.copyfile(vimrc_path, './vimrc.bak')
-else:
-    print('file:', vimrc_path, 'has NOT existsed')
-    print('create a new .vimrc in ~')
+# vimrc
+def vimrc_install(target_path, src_path):
+    if os.path.exists(target_path):
+        print('vimrc:', vimrc_path, 'Found')
+        print('vimrc: Back up it to vimrc/vimrc.bak')
+        shutil.copyfile(target_path, './backup/vimrc.bak')
+    else:
+        print('vimrc:', target_path, 'Not found')
+        print('vimrc: create a new .vimrc in ~')
+    
+    shutil.copyfile(src_path, target_path)
+    print('vimrc: done\n')
+    
+# directory colors
+def colors_install(target_path, src_path):
+    if not os.path.exists(target_path):
+        print('colors:', target_path, 'Not found')
+        print('colors: creating %s' % target_path)
+        os.makedirs(target_path)
+    else:
+        print('colors: Found')
+    
+    print('colors: coping some color.vim to %s' % target_path)
+    if src_path[-1] != '/':
+        src_path += '/'
+    for filename in os.listdir(src_path):
+        shutil.copy(src_path + filename, target_path)
+    
+    print('colors: done\n')
 
-shutil.copyfile('./vimrc', vimrc_path)
-print('done')
+# remove vundle code in vimrc
+def remove_vundle(vimrc_path, flag):
+    with open(vimrc_path, 'r+') as f:
+        data = f.read()
+        idx = data.find(flag)
+        if idx != -1:
+            print("remove vundle: flag '%s' Found" % flag)
+            # 去掉前面的vundle配置代码，+2是去掉FLAG后的两个回车
+            no_vundle = data[idx + len(FLAG)+2:]
+            f.seek(0) # 清空之前一定要先定位
+            f.truncate() # 清空文件
+            f.write(no_vundle)
+            print('remove Vundle: done')
+        else:
+            print("remove vundle: flag '%s' Not found" % flag)
 
-colors_path = '/home/{user}/.vim/colors'.format(user=getpass.getuser())
-if not os.path.exists(colors_path):
-    print('creating %s' % colors_path)
-    os.makedirs(colors_path)
+def ctags_install():
+    pass
 
-print('coping some color.vim to %s' % colors_path)
-for filename in os.listdir('./colors'):
-    shutil.copy('./colors/'+filename, colors_path)
 
-print('done')
+if __name__ == '__main__':
+    vimrc_path = '/home/{user}/.vimrc'.format(user=getpass.getuser())
+    vimrc_src_path = './src/vimrc'
+
+    colors_path = '/home/{user}/.vim/colors'.format(user=getpass.getuser())
+    colors_src_path = './colors'
+
+    vundle_path = '/home/{user}/.vim/bundle/Vundle.vim'.format(
+            user=getpass.getuser()
+        )
+
+    FLAG = '@Vundle end$'
+
+    vimrc_install(vimrc_path, vimrc_src_path)
+    colors_install(colors_path, colors_src_path)
+    yes_or_no = input('Do you want to install Vundle? [Y/N]: ')
+    if len(yes_or_no) > 0 and yes_or_no.lower()[0] == 'n':
+        remove_vundle(vimrc_path, FLAG)
+    else:
+        vundle_install(vundle_path)
+    
+    print('Enjoy!')
 
